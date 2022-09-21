b30a7636da8ae7b24e1c39f541be46d640eb004a
naruto <73755581+naruto-o@users.noreply.github.com>
2022-09-21 11:15:42 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,19 @@
+function first(){
+    console.log("first");
+}
+function seccond(){
+//     console.log('second');
+}
+function third(){
+    console.log('third');
+}
+first();
+seccond();
+third();
+// async for optemization
+let age = 25;
+const name = 'shikahr',
+letage =32, 
+lecture = 'full scripting with full stack ;'
+console.log(age);
+alert
