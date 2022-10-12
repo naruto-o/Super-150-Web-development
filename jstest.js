4347cab536ebeac0ae49f5dd1c2d9bd511b4ea35
naruto <73755581+naruto-o@users.noreply.github.com>
2022-10-12 10:51:25 +0530

    Add files via upload

---
+++ jstest.js
@@ -0,0 +1,25 @@
+
+let message2 = "i am 20years old";
+function abc(a,b){
+    let message2 = 'hey how u doing?';
+    let messsage = "my name is";
+    console.log(message2+a+b+messsage);
+}
+abc('john','doe');
+console.log(message2);
+
+
+
+function sum(c,d){
+    return c+d;
+}
+console.log(sum(11,34));
+
+
+let sayhi = function(){
+    console.log("hello world!!!")
+}
+sayhi();    
+let summing = sayhi;
+console.log(summing);
+
