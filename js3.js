fe270563b4ff2c02865fbc6c4386fa56e2eca630
naruto <73755581+naruto-o@users.noreply.github.com>
2022-10-12 10:53:03 +0530

    Add files via upload

---
+++ js3.js
@@ -0,0 +1,22 @@
+
+let num = 100_00_00;
+console.log(num);
+let num1 = 255;
+console.log(num1.toString(2));
+let num2= 11.57;
+console.log(Math.round(num2));
+let num3 =13.46;
+console.log(num3.toFixed(3));
+let num4 = 5;
+let num5 = 2;
+console.log(Math.pow(num4,num5));
+console.log(parseInt('100px'));
+console.log(parseFloat('12.55rem'));
+
+let str = " I'm this \n is a string";
+console.log(str);
+console.log(str.length);
+console.log(str[6]);
+console.log(str[str.length-1]);
+console.log(str.substring(3,1));
+console.log(str.slice(-3,-1));
