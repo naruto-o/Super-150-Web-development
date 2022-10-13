1cd55b8cbc47511d4cde34c9d3210ecf198db30b
naruto <73755581+naruto-o@users.noreply.github.com>
2022-10-13 11:55:17 +0530

    Add files via upload

---
+++ js5.js
@@ -0,0 +1,49 @@
+let arr1 = new Array(5);
+let arr = [1,2,3,4,5];
+console.log(arr);
+let arr2 = [1,2,3,4,5,console.log(arr),true,function(){ alert('hello world')},'mangoes','hewlett packard'];
+console.log(arr[9]);
+console.log(arr);
+console.log(arr2);
+console.log(arr.pop());
+console.log(arr.push());
+console.log(arr.shift());
+console.log(arr.unshift('23',67));
+console.log(arr);
+let arr3 = [];
+arr3[255] = 2;
+console.log(arr3);
+console.log(arr.length); 
+console.log("yo");
+let array= [[1,2,3],[4,5,6],[7,8,9]];
+// for (let index = 0; index < array.length; index++) {
+//     for (let j = 0; j < array[1].length; j++) {
+//         console.log(i,j);
+//         sum+= array[i][j];
+        
+//     }
+    let arr5 = ['mango','apples','banana'];
+    delete arr5[2];
+    arr5.splice[3,'grapes'];
+    console.log(arr5);
+
+        // const fruits = ["apple", "orange", "cherry"];
+        // fruits.forEach(myFunction);
+        // function myFunction(item, index) {
+        //     text += index + ": " + item + "<br>"; 
+        //   }
+        //   array.forEach(element => {
+        //     fruits;
+        //   });
+      let str = " this is a dev class";
+      let reversed = ""; 
+      function reverse(str){  
+         for (var i = str.length - 1; i >= 0; i--){         
+           reversed += str[i];  
+         }     
+        return reversed;
+      }
+      console.log(reversed);
+      property
+
+      
